ee513e8c9412facb90491be580e9d00cbef54a68
Tran Ngoc Bao <baotn@icd-vn.com>
2024-05-27 15:04:33 +0700

    update

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ wss.on("connection", function (ws) {
   clients.push({ id: client_uuid, ws: ws, nickname: nickname });
   console.log("client [%s] connected", client_uuid);
   ws.on("message", function (message) {
-    if (message.indexOf("/nick") == 0) {
-      var nickname_array = message.split(" ");
+    if (message.toString().indexOf("/nick") == 0) {
+      var nickname_array = message.toString().split(" ");
       if (nickname_array.length >= 2) {
         var old_nickname = nickname;
         nickname = nickname_array[1];
